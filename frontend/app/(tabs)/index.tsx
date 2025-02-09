bcd294861797f4badf3787f5b9555633e8b22f3e
Aqib Hassan <work.aqibhassan@gmail.com>
2025-02-09 20:38:40 +0500

    Pull

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React from 'react'
+
+
+const index = () => {
+  return (
+    <View className=''>
+      
+      <Button title="Click me"></Button>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
